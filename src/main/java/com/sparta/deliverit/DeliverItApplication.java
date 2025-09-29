c3c01d971c0406287c50b2c003a1cdb937b8c527
WisdomKim95 <aoghk3024@gmail.com>
2025-09-29 15:37:13 +0900

    init commit

---
+++ src/main/java/com/sparta/deliverit/DeliverItApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.deliverit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/deliverit/DeliverItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deliverit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
