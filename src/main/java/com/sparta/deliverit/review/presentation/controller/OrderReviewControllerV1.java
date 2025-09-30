1f97e5a15384089e7f24af31182a64b05c19705a
leemk09 <dlaudrb09@naver.com>
2025-09-30 15:42:47 +0900

    test: 주문 리뷰 조회

--- src/main/java/com/sparta/deliverit/review/presentation/controller/OrderReviewControllerV1.java
+++ src/main/java/com/sparta/deliverit/review/presentation/controller/OrderReviewControllerV1.java
@@ -2,13 +2,34 @@ package com.sparta.deliverit.review.presentation.controller;
 
 import com.sparta.deliverit.review.presentation.dto.request.CreateOrderReviewRequest;
 import com.sparta.deliverit.review.presentation.dto.response.MutateReviewResponse;
+import com.sparta.deliverit.review.presentation.dto.response.OrderReviewResponse;
+import com.sparta.deliverit.review.presentation.dto.response.ReviewResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/orders")
 public class OrderReviewControllerV1 {
+
+    @GetMapping("/{orderId}/reviews")
+    public ResponseEntity<OrderReviewResponse> getOrderReviews(
+            @PathVariable String orderId
+    ) {
+        return ResponseEntity.ok(new OrderReviewResponse(
+                List.of(
+                        new ReviewResponse(
+                                1L,
+                                "userId",
+                                BigDecimal.valueOf(4.5),
+                                "리뷰 설명"
+                        )
+                )
+        ));
+    }
 
     @PostMapping("/{orderId}/reviews")
     public ResponseEntity<MutateReviewResponse> create(
