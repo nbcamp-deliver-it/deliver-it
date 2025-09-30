3d019cc86a5fb844d7458d2456bea9b229f288ff
huhsiyoung <july433@naver.com>
2025-09-30 15:03:11 +0900

    chore: 불필요 어노테이션 제거

--- src/main/java/com/sparta/deliverit/exception/ErrorResponse.java
+++ src/main/java/com/sparta/deliverit/exception/ErrorResponse.java
@@ -1,11 +1,9 @@
 package com.sparta.deliverit.exception;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 @AllArgsConstructor
 public class ErrorResponse {
     private ErrorCode errorCode;
