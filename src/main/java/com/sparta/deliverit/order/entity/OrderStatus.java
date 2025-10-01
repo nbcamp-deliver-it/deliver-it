ba33c00b2e9811f3929cd32803025cd0e6b87348
yoo20370 <yoo20370@gmail.com>
2025-10-01 14:00:45 +0900

    feat(Order) : OrderStatus 결제 대기, 결제 완료 상태 추가

--- src/main/java/com/sparta/deliverit/order/entity/OrderStatus.java
+++ src/main/java/com/sparta/deliverit/order/entity/OrderStatus.java
@@ -2,6 +2,9 @@ package com.sparta.deliverit.order.entity;
 
 public enum OrderStatus {
     CREATED("주문 생성"),
+    PAYMENT_PENDING("결제 대기"),
+    PAYMENT_COMPLETED("결제 완료"),
+    PAYMENT_CANCEL("결제 완료"),
     CANCELED("주문 취소"),
     CONFIRMED("주문 확인");
 
