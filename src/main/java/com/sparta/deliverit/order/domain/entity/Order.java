941d3926f46b8a0a2c6992e21afbfb3d666bcf08
yoo20370 <yoo20370@gmail.com>
2025-10-03 02:01:17 +0900

    feat(Order) : order 엔티티 생성 및 연관관계 매핑

---
+++ src/main/java/com/sparta/deliverit/order/domain/entity/Order.java
@@ -0,0 +1,52 @@
+package com.sparta.deliverit.order.domain.entity;
+
+import com.sparta.deliverit.anything.entity.BaseEntity;
+import com.sparta.deliverit.payment.domain.entity.Payment;
+import com.sparta.deliverit.restaurant.domain.entity.Restaurant;
+import com.sparta.deliverit.user.domain.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "p_order")
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_id")
+    private String orderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_order_user"))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_order_restaurant"))
+    private Restaurant restaurant;
+
+    @OneToOne(mappedBy = "order", fetch = FetchType.LAZY)
+    private Payment payment;
+
+    @Column(name = "ordered_at", nullable = false, updatable = false)
+    LocalDateTime orderedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status", nullable = false)
+    private OrderStatus orderStatus;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "total_price", nullable = false, updatable = false)
+    private int totalPrice;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    protected Order() {
+
+    }
+}
