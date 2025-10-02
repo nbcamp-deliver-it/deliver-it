8c8f84f0c7c137131eb9eee19aff41af9fc6b528
huhsiyoung <july433@naver.com>
2025-10-02 14:36:23 +0900

    메뉴 서비스 어노테이션, 주석 수정

--- src/main/java/com/sparta/deliverit/menu/application/service/MenuService.java
+++ src/main/java/com/sparta/deliverit/menu/application/service/MenuService.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MenuService {
 
@@ -21,7 +22,6 @@ public class MenuService {
         return menuList.isEmpty() ? Collections.emptyList() : menuList;
     }
 
-    @Transactional
     public void createMenuItem(String restaurantId, List<Menu> menuList) {
 //        Restaurant restaurant = restaurantRepository.findById(restaurantId)
 //                .orElseThrow(() -> new IllegalArgumentException("해당 음식점이 존재하지 않습니다."));
@@ -33,22 +33,20 @@ public class MenuService {
         menuRepository.saveAll(menuList);
     }
 
-    @Transactional
     public void deleteMenuItem(String restaurantId, List<String> menuIdList) {
-
+        // 로직 추가 필요
     }
 
-    @Transactional
     public void updateMenuItem(String restaurantId, List<Menu> menuList) {
         for (Menu updatedMenu : menuList) {
             Menu existingMenu = menuRepository
                     .findById(updatedMenu.getId())
-                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴입니다."));
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴입니다.")); // 도메인 예외 필요함
 
             String id = existingMenu.getRestaurant().getRestaurantId();
 
             if (id.equals(restaurantId)) {
-                throw new IllegalArgumentException("해당 음식점의 메뉴가 아닙니다.");
+                throw new IllegalArgumentException("해당 음식점의 메뉴가 아닙니다."); // 도메인 예외 필요함
             }
 
             existingMenu.updateMenu(updatedMenu);
