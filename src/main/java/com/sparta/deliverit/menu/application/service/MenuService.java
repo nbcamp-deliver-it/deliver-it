961bb01f8fabccc5c3baf249ec40b694636b104e
huhsiyoung <july433@naver.com>
2025-10-02 17:28:43 +0900

    메뉴 엔티티 식당 연관관계 제거

--- src/main/java/com/sparta/deliverit/menu/application/service/MenuService.java
+++ src/main/java/com/sparta/deliverit/menu/application/service/MenuService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -17,19 +16,10 @@ public class MenuService {
     private final MenuRepository menuRepository;
 
     public List<Menu> getMenuByRestaurantId(String restaurantId) {
-        List<Menu> menuList = menuRepository.findAllByRestaurant_Id(restaurantId);
-
-        return menuList.isEmpty() ? Collections.emptyList() : menuList;
+        return null;
     }
 
     public void createMenuItem(String restaurantId, List<Menu> menuList) {
-//        Restaurant restaurant = restaurantRepository.findById(restaurantId)
-//                .orElseThrow(() -> new IllegalArgumentException("해당 음식점이 존재하지 않습니다."));
-//
-//        for (Menu menu : menuList) {
-//            menu.setRestaurant(restaurant); // FK 세팅
-//        }
-
         menuRepository.saveAll(menuList);
     }
 
@@ -38,20 +28,6 @@ public class MenuService {
     }
 
     public void updateMenuItem(String restaurantId, List<Menu> menuList) {
-        for (Menu updatedMenu : menuList) {
-            Menu existingMenu = menuRepository
-                    .findById(updatedMenu.getId())
-                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴입니다.")); // 도메인 예외 필요함
-
-            String id = existingMenu.getRestaurant().getRestaurantId();
-
-            if (id.equals(restaurantId)) {
-                throw new IllegalArgumentException("해당 음식점의 메뉴가 아닙니다."); // 도메인 예외 필요함
-            }
-
-            existingMenu.updateMenu(updatedMenu);
-
-            //  @Transactional dirty checking 으로 Menu 자동 업데이트
-        }
+        // 로직 추가 필요
     }
 }
