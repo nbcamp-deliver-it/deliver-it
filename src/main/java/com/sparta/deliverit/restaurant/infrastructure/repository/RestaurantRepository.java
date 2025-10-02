b82b86979d47f028b6bfbb777a4d8ba4879d86e8
YB-Taekwon <loundo001@gmail.com>
2025-10-02 15:14:38 +0900

    feat: 음식점 단일 조회, 수정, 삭제 기능 API 구현

--- src/main/java/com/sparta/deliverit/restaurant/infrastructure/repository/RestaurantRepository.java
+++ src/main/java/com/sparta/deliverit/restaurant/infrastructure/repository/RestaurantRepository.java
@@ -1,9 +1,14 @@
 package com.sparta.deliverit.restaurant.infrastructure.repository;
 
 import com.sparta.deliverit.restaurant.domain.entity.Restaurant;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    @EntityGraph(attributePaths = "categories")
+    Optional<Restaurant> findByRestaurantIdAndDeletedFalse(String restaurantId);
 }
