3dd842ffb3a1c3a5fad5631c4b93249c906cc418
yoo20370 <yoo20370@gmail.com>
2025-10-13 00:05:11 +0900

    feat(Order) : OrderRepository에 주문 목록 조회(고객, 점주)를 위한 쿼리 추가 및 테스트 코드 추가 - findOrdersByUserIdForUser() 고객 주문 목록 조회 구현 - findOrdersByRestaurantIdForOwner() 점주 주문 목록 조회 구현 - OrderRepositoryTest에서 주문 목록 조회(고객, 점주) 해피 케이스 테스트 코드 추가

--- src/main/java/com/sparta/deliverit/order/infrastructure/OrderRepository.java
+++ src/main/java/com/sparta/deliverit/order/infrastructure/OrderRepository.java
@@ -3,10 +3,13 @@ package com.sparta.deliverit.order.infrastructure;
 import com.sparta.deliverit.order.domain.entity.Order;
 import com.sparta.deliverit.order.infrastructure.dto.OrderDetailForOwner;
 import com.sparta.deliverit.order.infrastructure.dto.OrderDetailForUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
@@ -51,4 +54,60 @@ public interface OrderRepository extends JpaRepository<Order, String> {
         WHERE o.orderId =:orderId 
     """)
     Optional<OrderDetailForOwner> getByOrderIdForOwner(@Param("orderId") String orderId);
+
+    @Query(
+    """
+        SELECT 
+            u.id AS userId,
+            u.username AS userName,    
+            r.restaurantId AS restaurantId,
+            r.name AS restaurantName,
+            o.orderId AS orderId,
+            o.orderedAt AS orderedAt,
+            o.orderStatus AS orderStatus,
+            o.address AS address,
+            o.totalPrice AS totalPrice
+        FROM Order o
+        JOIN o.user u 
+        JOIN o.restaurant r
+        WHERE o.user.id = :userId
+            and o.orderedAt >= :from
+            and o.orderedAt < :to    
+        ORDER BY o.orderedAt DESC, o.orderId desc
+    """)
+    Page<OrderDetailForUser> findOrdersByUserId(
+            @Param("userId") String userId,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to,
+            Pageable pageable
+    );
+
+    @Query(
+    """
+        SELECT 
+            u.id AS userId,
+            u.username AS userName,    
+            r.restaurantId AS restaurantId,
+            r.name AS restaurantName,
+            r.user.id AS restaurantUserId,
+            o.orderId AS orderId,
+            o.orderedAt AS orderedAt,
+            o.orderStatus AS orderStatus,
+            o.address AS address,
+            o.totalPrice AS totalPrice,
+            o.version AS version
+        FROM Order o
+        JOIN o.user u 
+        JOIN o.restaurant r
+        WHERE r.restaurantId = :restaurantId
+            and o.orderedAt >= :from
+            and o.orderedAt < :to    
+        ORDER BY o.orderedAt DESC, o.orderId desc
+    """)
+    Page<OrderDetailForOwner> findOrdersByRestaurantIdForOwner(
+            @Param("restaurantId") String restaurantId,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to,
+            Pageable pageable
+    );
 }
--- src/test/java/com/sparta/deliverit/order/infrastructure/OrderRepositoryTest.java
+++ src/test/java/com/sparta/deliverit/order/infrastructure/OrderRepositoryTest.java
@@ -8,9 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
@@ -61,5 +65,39 @@ class OrderRepositoryTest {
             Assertions.assertThat(order.getTotalPrice()).isEqualByComparingTo("40000");
             Assertions.assertThat(order.getVersion()).isEqualByComparingTo(0L);
         });
+    }
+
+    @DisplayName("주문 목록 조회시, from 시간대부터 to 사이의 주문 목록을 가져온다. (from <= X < to) 이때, 정확히 to와 일치하는 주문과 이후 주문은 읽어오지 않는다.")
+    @Test
+    void findOrdersByUserIdTest() {
+        // when
+        Page<OrderDetailForUser> pageOrderList = orderRepository.findOrdersByUserIdForUser(
+                "1",
+                LocalDateTime.of(2025, 10, 1, 12, 0, 0),
+                LocalDateTime.of(2025, 10, 8, 12, 0, 0),
+                PageRequest.of(0, 10));
+
+        List<OrderDetailForUser> findOrderList = pageOrderList.getContent();
+        // then
+        Assertions.assertThat(findOrderList).size().isEqualTo(1);
+        Assertions.assertThat(findOrderList.get(0).getOrderId()).isEqualTo("00000000-0000-0000-0000-000000000001");
+        Assertions.assertThat(findOrderList.get(0).getTotalPrice()).isEqualByComparingTo("40000");
+    }
+
+    @DisplayName("음식점에서 주문 목록 조회시, from 시간대부터 to 사이의 주문 목록을 가져온다. (from <= X < to) 이때, 정확히 to와 일치하는 주문과 이후 주문은 읽어오지 않는다.")
+    @Test
+    void findOrdersByRestaurantIdTest() {
+        // when
+        Page<OrderDetailForOwner> pageOrderList = orderRepository.findOrdersByRestaurantIdForOwner(
+                "11111111-1111-1111-1111-111111111111",
+                LocalDateTime.of(2025, 10, 1, 12, 0, 0),
+                LocalDateTime.of(2025, 10, 8, 12, 0, 0),
+                PageRequest.of(0, 10));
+
+        List<OrderDetailForOwner> findOrderList = pageOrderList.getContent();
+        // then
+        Assertions.assertThat(findOrderList).size().isEqualTo(1);
+        Assertions.assertThat(findOrderList.get(0).getOrderId()).isEqualTo("00000000-0000-0000-0000-000000000001");
+        Assertions.assertThat(findOrderList.get(0).getTotalPrice()).isEqualByComparingTo("40000");
     }
 }
