40d5e444a162bfc4980b90e292eea5fb28acfbad
yoo20370 <yoo20370@gmail.com>
2025-10-12 22:34:46 +0900

    feat(Order) : OrderItem 엔티티 Getter 추가, 필드 추가 , Builder를 위한 생성자 추가, static create() 메서드 추가

--- src/main/java/com/sparta/deliverit/order/domain/entity/OrderItem.java
+++ src/main/java/com/sparta/deliverit/order/domain/entity/OrderItem.java
@@ -3,7 +3,12 @@ package com.sparta.deliverit.order.domain.entity;
 import com.sparta.deliverit.anything.entity.BaseEntity;
 import com.sparta.deliverit.menu.domain.entity.Menu;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 
+import java.math.BigDecimal;
+
+@Getter
 @Entity
 @Table(name = "p_order_item")
 public class OrderItem extends BaseEntity {
@@ -20,10 +25,37 @@ public class OrderItem extends BaseEntity {
     @JoinColumn(name = "menu_id", nullable = false, foreignKey = @ForeignKey(name = "fk_order_item_menu"))
     private Menu menu;
 
+    @Column(name = "menu_name_snapshot", nullable = false)
+    private String menuNameSnapshot;
+
+    @Column(name = "menu_price_snapshot", nullable = false)
+    private BigDecimal menuPriceSnapshop;
+
     @Column(nullable = false)
     private int quantity;
 
     protected OrderItem() {
 
+    }
+
+    @Builder
+    private OrderItem(String orderItemId, Order order, Menu menu, String menuNameSnapshot, BigDecimal menuPriceSnapshop, int quantity) {
+        this.orderItemId = orderItemId;
+        this.order = order;
+        this.menu = menu;
+        this.menuNameSnapshot = menuNameSnapshot;
+        this.menuPriceSnapshop = menuPriceSnapshop;
+        this.quantity = quantity;
+    }
+
+    public static OrderItem create(String orderItemId, Order order, Menu menu, String menuNameSnapshot, BigDecimal menuPriceSnapshop, int quantiy) {
+        return OrderItem.builder()
+                .orderItemId(orderItemId)
+                .order(order)
+                .menu(menu)
+                .menuNameSnapshot(menuNameSnapshot)
+                .menuPriceSnapshop(menuPriceSnapshop)
+                .quantity(quantiy)
+                .build();
     }
 }
