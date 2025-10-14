91dbfb1ee5f225153f3eb3a7699d5d26680a2568
YB-Taekwon <loundo001@gmail.com>
2025-10-15 05:38:20 +0900

    feat: DTO 수정

--- src/main/java/com/sparta/deliverit/restaurant/presentation/dto/RestaurantInfoRequestDto.java
+++ src/main/java/com/sparta/deliverit/restaurant/presentation/dto/RestaurantInfoRequestDto.java
@@ -16,21 +16,23 @@ import java.util.List;
 @Builder
 public class RestaurantInfoRequestDto {
 
-    @NotBlank
+    @NotBlank(message = "음식점 이름이 비어있습니다.")
     private String name;
 
-    @NotBlank
+    @NotBlank(message = "음식점 전화번호가 비어있습니다.")
     private String phone;
 
-    @NotBlank
+    @NotBlank(message = "음식점 주소가 비어있습니다.")
     private String address;
 
-    @NotBlank
+    @NotBlank(message = "음식점 소개글이 비어있습니다.")
     private String description;
 
-    @NotNull
+    @NotNull(message = "음식점 상태가 NULL 입니다.")
     private RestaurantStatus status;
 
-    @NotEmpty
+    @NotEmpty(message = "음식점 카테고리가 비어있습니다.")
     private List<RestaurantCategory> categories;
+
+    private String ownerId; // 가게를 관리자(MANAGER, MASTER)가 저장할 때 사용
 }
--- src/main/java/com/sparta/deliverit/restaurant/presentation/dto/RestaurantListResponseDto.java
+++ src/main/java/com/sparta/deliverit/restaurant/presentation/dto/RestaurantListResponseDto.java
@@ -14,4 +14,15 @@ public class RestaurantListResponseDto {
     private String name;
     private BigDecimal rating;
     private Long reviewCount;
+    private Double distance; // 별점순 정렬 시 NULL
+
+    // 별점순 정렬 시 사용
+    public RestaurantListResponseDto(
+            String restaurantId, String name, BigDecimal rating, Long reviewCount
+    ) {
+        this.restaurantId = restaurantId;
+        this.name = name;
+        this.rating = rating;
+        this.reviewCount = reviewCount;
+    }
 }
