6ae7d02798714e5546c775eae8231dc17c42cbe0
YB-Taekwon <loundo001@gmail.com>
2025-10-15 19:29:58 +0900

    feat: Controller - getRestaurantList 메서드 시그니처 수정 (Pageable 생성 방식 변경)

--- src/main/java/com/sparta/deliverit/restaurant/presentation/controller/RestaurantController.java
+++ src/main/java/com/sparta/deliverit/restaurant/presentation/controller/RestaurantController.java
@@ -8,9 +8,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -41,13 +38,11 @@ public class RestaurantController {
     // 음식점 전체 목록 조회
     @GetMapping
     public ResponseEntity<Page<RestaurantListResponseDto>> getRestaurantList(
-            @Valid @ModelAttribute RestaurantListRequestDto requestDto,
-            @PageableDefault(size = 20, sort = "distance", direction = Sort.Direction.ASC) Pageable pageable
-    ) {
+            @Valid @ModelAttribute RestaurantListRequestDto requestDto) {
         log.info("Controller - getAllRestaurants 실행: latitude={}, longitude={}, keyword={}, category={}",
                 requestDto.getLatitude(), requestDto.getLongitude(), requestDto.getKeyword(), requestDto.getCategory());
 
-        Page<RestaurantListResponseDto> restaurantList = restaurantService.getRestaurantList(requestDto, pageable);
+        Page<RestaurantListResponseDto> restaurantList = restaurantService.getRestaurantList(requestDto);
 
         log.info("Controller - getAllRestaurants 종료: restaurantList Total Page={}", restaurantList.getTotalPages());
         return ResponseEntity.ok(restaurantList);
