0a6d54dfb15feb7edf125534ef382ba925e43bc6
yoo20370 <yoo20370@gmail.com>
2025-10-17 01:51:31 +0900

    feat(Order) : OrderRepository에 주문 취소를 진행해야 하는 주문 ID를 찾는 쿼리, 특정 주문을 취소 상태로 변경하는 메서드 추가

--- src/main/java/com/sparta/deliverit/order/infrastructure/dto/OrderIdVersion.java
+++ src/main/java/com/sparta/deliverit/order/infrastructure/dto/OrderIdVersion.java
@@ -1,4 +1,9 @@
 package com.sparta.deliverit.order.infrastructure.dto;
 
+import java.time.LocalDateTime;
+
 public interface OrderIdVersion {
+    String getOrderId();
+    Long getVersion();
+    LocalDateTime getOrderedAt();
 }
