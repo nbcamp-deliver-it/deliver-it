544f380fa84a20118424a411edb9e03e78422bd5
huhsiyoung <july433@naver.com>
2025-10-16 11:36:50 +0900

    feat: RestaurantRepository existsByRestaurantId 메서드 추가

--- src/main/java/com/sparta/deliverit/restaurant/infrastructure/repository/RestaurantRepository.java
+++ src/main/java/com/sparta/deliverit/restaurant/infrastructure/repository/RestaurantRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
     @EntityGraph(attributePaths = "categories")
     Optional<Restaurant> findByRestaurantIdAndDeletedFalse(String restaurantId);
+    Boolean existsByRestaurantId(String restaurantId);
 }
